84c631be24303f8234507657aaef33dffe403654
AhmAlgiz <aaa.01a@yandex.ru>
2022-05-18 00:04:31 +0300

    1st

---
+++ src/main/java/com/example/springboot/Controllers/ImgReceiverController.java
@@ -0,0 +1,48 @@
+package com.example.springboot.Controllers;
+
+import com.example.springboot.Services.ImageService;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Map;
+
+@RestController
+public class ImgReceiverController {
+
+    private ImageService imageService;
+
+    public ImgReceiverController(ImageService imageService) {
+        this.imageService = imageService;
+    }
+
+    @PostMapping("/storePhoto")
+    @ResponseBody
+    public ResponseEntity<String> storePhoto
+            (@RequestParam("file") MultipartFile file) {
+        return new ResponseEntity(imageService.StorePhoto(file), HttpStatus.OK);
+    }
+    @GetMapping("/getLastPhoto")
+    public ResponseEntity<InputStreamResource> getLastPhoto (){
+        return imageService.GetPhoto();
+    }
+
+    @PostMapping("/sendResponse")
+    @ResponseBody
+    public ResponseEntity takeResponse
+            (@RequestParam("response") String response) {
+        imageService.StoreResponse(response);
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @GetMapping("/getResponse/")
+    @ResponseBody
+    public ResponseEntity<String> getResponse(@RequestParam("filename") String filename) {
+        return new ResponseEntity(imageService.GetResponse(filename), HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/example/springboot/Services/ImageService.java
@@ -0,0 +1,80 @@
+package com.example.springboot.Services;
+
+import com.example.springboot.Exceptions.StorageException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.tomcat.util.json.JSONParser;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+
+@Service
+public class ImageService {
+
+    private HashMap<String, String> scanResponses = new HashMap<>();
+    private List<String> filenames = new ArrayList<>();
+
+
+    public ResponseEntity<InputStreamResource> GetPhoto () {
+        return SendJPEGAsResponseEntity(filenames.get(filenames.size()-1));
+    }
+
+    public String StorePhoto(MultipartFile file) {
+        return storeJPEG(file);
+    }
+
+    public void StoreResponse(String response) {
+        scanResponses.put(filenames.get(filenames.size()-1), response);
+    }
+
+    public String GetResponse(String filename) {
+        return scanResponses.get(filename);
+    }
+
+    private String storeJPEG(MultipartFile file) {
+        String filename = StringUtils.cleanPath(file.getOriginalFilename());
+        try {
+            if (file.isEmpty()) {
+                throw new StorageException("Failed to store empty file " + filename);
+            }
+            try (InputStream inputStream = file.getInputStream()) {
+                Files.copy(inputStream,
+                        Paths.get("src","main","resources","ImagesToScan", filename), REPLACE_EXISTING);
+            }
+        }
+        catch (IOException e) {
+            throw new StorageException("Failed to store file " + filename, e);
+        }
+        filenames.add(filename);
+        return filename;
+    }
+
+    private ResponseEntity<InputStreamResource> SendJPEGAsResponseEntity(String filename) {
+        try {
+            var imgFile = new ClassPathResource("ImagesToScan/" + filename);
+            return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG)
+                    .body(new InputStreamResource(imgFile.getInputStream()));
+        } catch (IOException e) {
+            throw new StorageException("System Error with file" + filename, e);
+        }
+    }
+}
